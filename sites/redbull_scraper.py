bec5131af139cc4d324d5484fec26afa0b59cf4c
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-10-11 11:21:57 +0300

    Create redbull_scraper.py

---
+++ sites/redbull_scraper.py
@@ -0,0 +1,48 @@
+#
+#  Company - > RedBull
+# Link -> https://jobs.redbull.com/ro-ro/results?locations=6188&locationNames=Rom%C3%A2nia&functions=&functionNames=&keywords=
+#
+from A_OO_get_post_soup_update_dec import update_peviitor_api
+from L_00_logo import update_logo
+import requests
+import uuid
+
+def get_jobs():
+    '''
+    This function collects data from the company site.
+    '''
+
+    response = requests.get('https://jobs.redbull.com/api/search?locations=6188&functions=&keywords=&pageSize=10&locale=ro&country=ro',
+                            headers={
+                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+                            }).json()['jobs']
+    list_jobs = []
+
+    for job in response:
+
+        list_jobs.append({
+            "id": str(uuid.uuid4()),
+            "job_title": job['title'],
+            "job_link": 'https://jobs.redbull.com/ro-ro/' + job['slug'],
+            "company": "RedBull",
+            "country": "Romania",
+            "city": job['locationText'].split(',')[0]
+        })
+
+    return list_jobs
+
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+company_name = 'RedBull'
+data_list = get_jobs()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('RedBull',
+                  'https://resources.redbull.com/logos/redbullcom/v3/redbullcom-logo.svg'
+                  ))
