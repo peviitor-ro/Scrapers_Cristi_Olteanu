80eaa370b9d6516d074a65876c5a80538a270bac
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-10-11 15:57:52 +0300

    added ceragon and caseware

---
+++ sites/caseware_scraper.py
@@ -0,0 +1,53 @@
+#
+# Company - > caseware
+# Link -> https://jobs.lever.co/caseware?location=Cluj%2C%20Romania
+#
+from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
+from L_00_logo import update_logo
+from bs4 import BeautifulSoup
+import requests
+import uuid
+
+def get_jobs():
+
+    req = requests.get('https://jobs.lever.co/caseware?location=Cluj%2C%20Romania',headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(req.text,'lxml')
+    jobs = soup.find_all('div',class_='posting')
+
+    list_jobs=[]
+
+    for job in jobs:
+
+        link = job.find('a',class_='posting-title')['href']
+        title = job.find('h5').text
+        city = job.find('span',class_='sort-by-location posting-category small-category-label location').text.split(',')[0]
+        type = job.find('span',class_='display-inline-block small-category-label workplaceTypes').text
+
+        list_jobs.append({
+            "id": str(uuid.uuid4()),
+            "job_title": title,
+            "job_link": link,
+            "company": "caseware",
+            "country": "Romania",
+            "city": city,
+            "remote": type
+        })
+
+    return list_jobs
+
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'caseware'  # add test comment
+data_list = get_jobs()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('caseware',
+                  'https://mma.prnewswire.com/media/1905723/CaseWare_International_Inc__Caseware_Expands_Global_Footprint_wi.jpg?w=200'
+                  ))
---
+++ sites/ceragon_scraper.py
@@ -0,0 +1,53 @@
+#
+# Company - > Ceragon
+# Link -> https://www.ceragon.com/about-ceragon/careers
+#
+from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
+from L_00_logo import update_logo
+from bs4 import BeautifulSoup
+import requests
+import uuid
+
+def get_jobs():
+
+    req = requests.get('https://www.ceragon.com/about-ceragon/careers',headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(req.text,'lxml')
+    jobs = soup.find_all('div',class_='row')
+
+    list_jobs=[]
+
+    for job in jobs:
+
+        link = 'https://www.ceragon.com' + job.find('a')['href']
+        title = job.find('h5').text
+        city = job.find('li',class_='hs-data-location').text.split()[1]
+        country = job.find('li',class_='hs-data-location').text.split()[0]
+
+        if country == 'Romania':
+            list_jobs.append({
+                "id": str(uuid.uuid4()),
+                "job_title": title,
+                "job_link": link,
+                "company": "Ceragon",
+                "country": "Romania",
+                "city": city,
+            })
+    return list_jobs
+
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'Ceragon'  # add test comment
+data_list = get_jobs()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('Ceragon',
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Ceragon_logo.svg/180px-Ceragon_logo.svg.png'
+                  ))
+
