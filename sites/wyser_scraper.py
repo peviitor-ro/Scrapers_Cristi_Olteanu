83944069da94d64267d6ab457e4067c6e5b89cfd
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-10-22 20:46:35 +0300

    Create wyser_scraper.py

---
+++ sites/wyser_scraper.py
@@ -0,0 +1,52 @@
+#
+#  Company - > wyser
+# Link -> https://ro.wyser-search.com/careers/
+#
+from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
+from L_00_logo import update_logo
+import requests
+from bs4 import BeautifulSoup
+import uuid
+
+
+def get_jobs():
+
+    list_jobs = []
+
+    response = requests.get(url='https://ro.wyser-search.com/job-offers/',
+                            headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    jobs = soup.find_all('article',class_='px-0 col-12 col-sm-6 col-xl-4 py-3 card-job')
+
+    for job in jobs:
+        link = job.find('a',class_='dettaglio')['href']
+        title = job.find('div',class_='col-10').text.strip()
+        city = job.find('li',class_='card-text size-16 blue font-weight-bold posto').text.split(',')[0]
+
+        list_jobs.append({
+            "id": str(uuid.uuid4()),
+            "job_title": title,
+            "job_link": link,
+            "company": "wyser",
+            "country": "Romania",
+            "city": city
+        })
+    return list_jobs
+
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'wyser'
+data_list = get_jobs()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('wyser',
+                  'https://ro.mywyser.com/assets/images/logo-wyser.png'
+                  ))
