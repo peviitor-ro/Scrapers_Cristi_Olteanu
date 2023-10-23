9e01045b9a4e1230e0b8483e243713dee19eedad
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-10-23 16:54:37 +0300

    Create fotc_scraper.py

---
+++ sites/fotc_scraper.py
@@ -0,0 +1,60 @@
+#
+#  Company - > fotc
+# Link -> https://fotc.jobsoid.com/
+#
+from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
+from L_00_logo import update_logo
+import requests
+from bs4 import BeautifulSoup
+import uuid
+
+def get_jobs():
+
+    list_jobs = []
+    response = requests.get('https://fotc.jobsoid.com/',
+                            headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text,'lxml')
+    jobs = soup.find_all('div',class_='row')
+
+    for job in jobs:
+        text = job.find('a',class_='jobDetailsLink')
+        try:
+            link = 'https://fotc.jobsoid.com'+text.get('href')
+        except:
+            link = None
+
+        if link != None:
+            title = text.text
+            link = link
+            location = job.find('span',class_='r-space').text.split('/')[0].strip()
+            type = job.find('span',class_='r-space').text.split('/')[-1].strip()
+
+            if 'Romania' in location:
+
+                list_jobs.append({
+                    "id": str(uuid.uuid4()),
+                    "job_title": title,
+                    "job_link": link,
+                    "company": "fotc",
+                    "country": "Romania",
+                    "city": '',
+                    "remote": type
+                })
+    return list_jobs
+
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'fotc'
+data_list = get_jobs()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('fotc',
+                  'https://fotc.jobsoid.com/PortalJob/GetPortalLogo?size=medium'
+                  ))
