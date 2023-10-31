d17897f5c924508cf67fa97b68b8f484fa51f187
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-10-31 20:07:59 +0200

    Update adecco_scraper.py

--- sites/adecco_scraper.py
+++ sites/adecco_scraper.py
@@ -8,19 +8,21 @@ from bs4 import BeautifulSoup
 import requests
 import uuid
 
+
 def get_soup(url: str):
 
     response = requests.get(url,headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text,'lxml')
     return soup
 
+
 def get_nr_pages():
 
-    soup_nr= get_soup('https://www.adecco.ro/jobs/?page=0')
-    nr_jobs = int(soup_nr.find('h1',class_='h6').text.split()[0])
+    soup_nr = get_soup('https://www.adecco.ro/jobs/?page=0')
+    nr_jobs = int(soup_nr.find('h1', class_='h6').text.split()[0])
     pages = int(nr_jobs/10)
     last_page = nr_jobs/10
-    if last_page>0:
+    if last_page > 0:
         nr_pages = pages+1
     else:
         nr_pages = pages
@@ -30,15 +32,30 @@ def get_nr_pages():
 def get_jobs():
     list_jobs = []
 
-    for page in range(1,get_nr_pages()+1,1):
+    for page in range(1, get_nr_pages()+1, 1):
 
-        soup_jobs= get_soup(url='https://www.adecco.ro/jobs/?page='+str(page))
-        jobs = soup_jobs.find_all('div',class_='card no-side-padding-m')
+        soup_jobs = get_soup(url='https://www.adecco.ro/jobs/?page=' + str(page))
+        jobs = soup_jobs.find_all('div', class_='card no-side-padding-m')
 
         for job in jobs:
-            city = job.find('div',class_='card__details').text.split()[0].strip(',')
+            city = job.find('ul', class_='list-unstyled').find('li').text.split(',')[0].strip()
             link = 'https://www.adecco.ro/jobs/'+job.find('a')['href']
             title = job.find('a').text
+
+            if 'remote' in city.lower() or 'remote' in title.lower():
+                city = ''
+                job_type = 'remote'
+            else:
+                job_type = 'on-site'
+
+            if 'Com' in city:
+                city = city.split()[0]
+            elif 'Bucuresti' in city or '104H' in city:
+                city = 'Bucuresti'
+            elif 'Mures' in city:
+                city = 'Targu Mures'
+            elif 'stefanestii' in city.lower():
+                city = 'Stefanesti de Jos'
 
             list_jobs.append({
                 "id": str(uuid.uuid4()),
@@ -50,6 +67,8 @@ def get_jobs():
             })
 
     return list_jobs
+
+get_jobs()
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
