390d6b0c2b0890d9e917a2e5361e879b852829d9
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-10-31 18:08:49 +0200

    Update massmutual_scraper.py

--- sites/massmutual_scraper.py
+++ sites/massmutual_scraper.py
@@ -2,36 +2,37 @@
 #  Company - > MassMutual
 # Link -> https://careers.massmutualromania.com/search-jobs
 #
-from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
+from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
 import uuid
 
+
 def get_jobs():
 
-    list_jobs= []
+    list_jobs = []
 
-    response = requests.get('https://careers.massmutualromania.com/search-jobs/results?ActiveFacetID=0&CurrentPage=1&RecordsPerPage=46&Distance=50&RadiusUnitType=0&Keywords=&Location=&ShowRadius=False&IsPagination=False&CustomFacetName=&FacetTerm=&FacetType=0&SearchResultsModuleName=Search+Results&SearchFiltersModuleName=Search+Filters&SortCriteria=0&SortDirection=0&SearchType=5&PostalCode=&ResultsType=0&fc=&fl=&fcf=&afc=&afl=&afcf=',headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
+    response = requests.get(
+        'https://careers.massmutualromania.com/search-jobs/results?ActiveFacetID=0&CurrentPage=1&RecordsPerPage=41&Distance=50&RadiusUnitType=0&Keywords=&Location=&ShowRadius=False&IsPagination=False&CustomFacetName=&FacetTerm=&FacetType=0&SearchResultsModuleName=Search+Results&SearchFiltersModuleName=Search+Filters&SortCriteria=0&SortDirection=0&SearchType=5&PostalCode=&ResultsType=0&fc=&fl=&fcf=&afc=&afl=&afcf=',
+        headers=DEFAULT_HEADERS).json()['results']
 
+    soup = BeautifulSoup(response, 'lxml')
     jobs = soup.find_all('li')
 
     for job in jobs:
-        text = job.find('h2',attrs={})
-        if text != None:
-            title = text.text
-            link = 'https://careers.massmutualromania.com/' + job.findNext('a')['href'].split('\"/')[1].strip('\\"')
-            city = job.findNext('span').text.split(',')[0]
+        link = 'https://careers.massmutualromania.com' + job.find('a')['href']
+        title = job.find('a').text
+        city = job.find('span', class_='job-location').text.split(',')[0].strip()
 
-            list_jobs.append({
-                "id": str(uuid.uuid4()),
-                "job_title": title,
-                "job_link": link,
-                "company": "MassMutual",
-                "country": "Romania",
-                "city": city
-                })
+        list_jobs.append({
+            "id": str(uuid.uuid4()),
+            "job_title": title,
+            "job_link": link,
+            "company": "MassMutual",
+            "country": "Romania",
+            "city": city
+        })
 
     return list_jobs
 
@@ -40,7 +41,6 @@ def scrape_and_update_peviitor(company_name, data_list):
     """
     Update data on peviitor API!
     """
-
     return data_list
 
 company_name = 'MassMutual'
