c7d3f449f7fcf7afb1ad1e4b4400fdf210bb7e97
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-11-22 17:06:32 +0200

    Update leroymerlin_scraper.py

--- sites/leroymerlin_scraper.py
+++ sites/leroymerlin_scraper.py
@@ -4,57 +4,57 @@
 #
 import requests
 from bs4 import BeautifulSoup
-from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
+from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
 from L_00_logo import update_logo
-import uuid
 
 
 def get_soup(url):
 
-    response = requests.get(url,headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
+    response = requests.get(url, headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
     return soup
+
 
 def get_pages():
 
     soup_pages = get_soup('https://job.leroymerlin.ro/jobs')
-    nr_jobs = int(soup_pages.find('span',class_='text-lg font-medium').text.split()[0])
+    nr_jobs = int(soup_pages.find('span', class_='text-lg font-medium').text.split()[0])
     pages = int(nr_jobs/20)
-    if nr_jobs%20>0:
+    if nr_jobs % 20 > 0:
         pages += 1
     return pages
+
 
 def get_jobs():
 
     list_jobs = []
 
-    for page in range(1,get_pages()+1,1):
+    for page in range(1, get_pages()+1, 1):
         soup_jobs = get_soup(f'https://job.leroymerlin.ro/jobs?page={page}')
 
-        jobs = soup_jobs.find_all('a',class_='block h-full w-full hover:bg-company-primary-text hover:bg-opacity-3 overflow-hidden group')
+        jobs = soup_jobs.find_all('a', class_='block h-full w-full hover:bg-company-primary-text hover:bg-opacity-3 overflow-hidden group')
 
         for job in jobs:
             link = job.get('href')
-            title = job.find('span',class_='text-block-base-link company-link-style').text
-            location = job.find('div',class_='mt-1 text-md').text.split('·')[1].strip()
+            title = job.find('span', class_='text-block-base-link company-link-style').text
+            location = job.find('div', class_='mt-1 text-md').text.split('·')[1].strip()
             if 'Târgu Mureș' in location:
-                city = 'Târgu Mureș'
+                city = 'Târgu-Mureș'
             else:
                 city = location.split()[0]
 
             try:
-                type = job.find('span',class_='inline-flex items-center gap-x-2').text.split()[-1]
+                job_type = job.find('span',class_='inline-flex items-center gap-x-2').text.split()[-1]
             except:
-                type = 'on-site'
+                job_type = 'on-site'
 
             list_jobs.append({
-                "id": str(uuid.uuid4()),
                 "job_title": title,
                 "job_link": link,
                 "company": "LeroyMerlin",
                 "country": "Romania",
                 "city": city,
-                "remote": type
+                "remote": job_type
             })
     return list_jobs
 
