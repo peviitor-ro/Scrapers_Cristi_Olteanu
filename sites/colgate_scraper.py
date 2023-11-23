f62672a6c7441aedb5f224888c7ecfa1f0043f2f
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-11-23 16:45:53 +0200

    Update colgate_scraper.py

--- sites/colgate_scraper.py
+++ sites/colgate_scraper.py
@@ -6,36 +6,43 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
+
+def get_soup(url):
+
+    response = requests.get(url, headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
 
 def get_jobs():
-    """
-              ... this func() make a simple requests
-              and collect data from Colgate API.
-           """
 
-    response = requests.get(
-        'https://jobs.colgate.com/go/View-All-Jobs/8506400/?q=&q2=&alertId=&locationsearch=&title=&location=RO&department=&facility=',headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text, 'lxml')
-    list_jobs = []
+    jobs_list = []
 
-    jobs = soup.find_all('tr',class_='data-row')
+    jobs = get_soup('https://jobs.colgate.com/go/View-All-Jobs/8506400/?q=&q2=&alertId=&locationsearch=&title=&location=RO&department=&facility='
+                    ).find_all('tr', class_='data-row')
 
     for job in jobs:
-        link = job.find('a',class_='jobTitle-link')['href']
-        city = job.find('span',class_='jobLocation').text.split(', ')[0].strip()
-        title = job.find('a',class_='jobTitle-link').text
+        link = 'https://jobs.colgate.com/' + job.find('a', class_='jobTitle-link')['href']
+        city = job.find('span', class_='jobLocation').text.split(', ')[0].strip()
+        title = job.find('a', class_='jobTitle-link').text
 
-        list_jobs.append({
-            "id": str(uuid.uuid4()),
+        try:
+            job_type = get_soup(link).find('span', {'data-careersite-propertyid': 'customfield5'}
+                                           ).text.strip()
+        except:
+            job_type = 'on-site'
+
+        jobs_list.append({
             "job_title": title,
-            "job_link": 'https://jobs.colgate.com/'+link,
+            "job_link": link,
             "company": "Colgate",
             "country": "Romania",
-            "city": city
+            "city": city,
+            "remote": job_type
         })
 
-    return list_jobs
+    return jobs_list
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
