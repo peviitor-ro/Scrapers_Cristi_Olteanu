9167f2e0029b99c1dbcef7949f45305b8d2adbbf
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-11-25 09:55:50 +0200

    Update olx_scraper.py

--- sites/olx_scraper.py
+++ sites/olx_scraper.py
@@ -6,38 +6,42 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
 
 
 def get_jobs():
+
+    list_jobs = []
     response = requests.get('https://jobs.eu.lever.co/olx',headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
-    list_jobs = []
 
     jobs = soup.find_all('div',class_='posting')
 
     for job in jobs:
-        link = job.find('a',class_='posting-title')['href']
+        link = job.find('a', class_='posting-title')['href']
         title = job.find('h5').text
-        country = job.find('span',class_='sort-by-location posting-category small-category-label location').text.split(', ')[-1]
-        location = job.find('span',class_='sort-by-location posting-category small-category-label location').text.split(', ')[0]
+        country = job.find('span',  class_='sort-by-location posting-category small-category-label location'
+                           ).text.split(', ')[-1]
+        location = job.find('span', class_='sort-by-location posting-category small-category-label location'
+                            ).text.split(', ')[0]
 
         if 'Remote' in location:
             city = 'Bucuresti'
-            remote = 'remote'
+            job_type = 'remote'
+        elif 'Hybrid' in location:
+            city = 'Bucuresti'
+            job_type = 'Hybrid'
         else:
-            city = location
-            remote = 'on-site'
+            city = 'Bucuresti'
+            job_type = 'on-site'
 
         if country == 'Romania':
             list_jobs.append({
-                "id": str(uuid.uuid4()),
                 "job_title": title,
                 "job_link": link,
                 "company": "OlxGroup",
                 "country": "Romania",
                 "city": city,
-                "remote": remote
+                "remote": job_type
             })
     return list_jobs
 
