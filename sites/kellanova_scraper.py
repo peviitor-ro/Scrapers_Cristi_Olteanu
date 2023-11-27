4f16246a2825530e041c77a71bbd74f858c75e67
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-11-27 19:10:18 +0200

    Update kellanova_scraper.py

--- sites/kellanova_scraper.py
+++ sites/kellanova_scraper.py
@@ -7,32 +7,37 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
+
+def get_soup(url):
+
+    r = requests.get(url, headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(r.text, 'lxml')
+    return soup
+
 
 def get_jobs():
     list_jobs = []
 
-    response = requests.get(
-        'https://jobs.kellanova.com/search/?createNewAlert=false&q=&locationsearch=Romania&optionsFacetsDD_department=&optionsFacetsDD_country=',
-        headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    jobs = soup.find_all('tr', class_='data-row')
+    jobs = get_soup('https://jobs.kellanova.com/search/?createNewAlert=false&q=&locationsearch=Romania&optionsFacetsDD_department=&optionsFacetsDD_country='
+                    ).find_all('tr', class_='data-row')
 
     for job in jobs:
         link = 'https://jobs.kellanova.com' + job.find('a', class_='jobTitle-link')['href']
         title = job.find('a', class_='jobTitle-link').text
         city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
+        try:
+            job_type = get_soup(link).find('span', attrs={'data-careersite-propertyid': 'shifttype'}).text.strip()
+        except:
+            job_type = 'on-site'
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "kellanova",
             "country": "Romania",
             "city": city,
         })
-
     return list_jobs
 
 @update_peviitor_api
