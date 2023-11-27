3027b8f1ab19d5d9cd42b97b85adc205c41678d4
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-11-27 19:36:39 +0200

    Update konecranes_scraper.py

--- sites/konecranes_scraper.py
+++ sites/konecranes_scraper.py
@@ -6,29 +6,37 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
+
+def get_soup(url):
+
+    r = requests.get(url, headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(r.text, 'lxml')
+    return soup
 
 
 def get_jobs():
 
     list_jobs = []
-    response = requests.get('https://careers.konecranes.com/Konecranes/search/?createNewAlert=false&q=&locationsearch=Romania',
-                            headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
-    jobs = soup.find_all('tr', class_='data-row')
+    jobs = get_soup('https://careers.konecranes.com/Konecranes/search/?createNewAlert=false&q=&locationsearch=Romania'
+                    ).find_all('tr', class_='data-row')
 
     for job in jobs:
         link = 'https://careers.konecranes.com' + job.find('a', class_='jobTitle-link')['href']
         city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
         title = job.find('a', class_='jobTitle-link').text
-
+        try:
+            job_type = get_soup(link).find('span', attrs={'data-careersite-propertyid': 'customfield5'}).text.strip()
+        except:
+            job_type = 'on-site'
+        print(job_type)
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "konecranes",
             "country": "Romania",
             "city": city,
+            "remote": job_type
         })
     return list_jobs
 
@@ -37,9 +45,7 @@ def scrape_and_update_peviitor(company_name, data_list):
     """
     Update data on peviitor API!
     """
-
     return data_list
-
 
 company_name = 'konecranes'  # add test comment
 data_list = get_jobs()
