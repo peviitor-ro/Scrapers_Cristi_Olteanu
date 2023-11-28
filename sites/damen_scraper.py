d2948bf2e001d4b9729b5a18df022ea967e84aed
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-11-28 13:55:37 +0200

    Update damen_scraper.py

--- sites/damen_scraper.py
+++ sites/damen_scraper.py
@@ -13,18 +13,18 @@ session = requests.Session()
 
 def get_cookies():
 
-    response = session.head(
+    response = requests.head(
         url='https://career.damen.com/wp-admin/admin-ajax.php',
     ).headers
     phpsessid = re.search(r"PHPSESSID=([^;]+);", str(response)).group(0)
     return phpsessid
 
 
-def prepare_post():
+def prepare_post(value):
 
     url = "https://career.damen.com/wp-admin/admin-ajax.php"
 
-    payload = ("action=lumesse_ajax_grid_list&"
+    payload = (f"action=lumesse_ajax_grid_list&"
                "lanugage=1&params%5B0%5D%5Bkey%"
                "5D=customField1&params"
                "%5B0%5D%5Bval"
@@ -32,7 +32,7 @@ def prepare_post():
                "5D=date&params%5B1%5D%5Bval%5D=all&params%5B2%5D%5Bkey"
                "%5D=keywords&params%5B2%5D%5Bval%5D=&params%5B3%5D%5Bkey"
                "%5D=sort&params%5B3%5D%5Bval%5D=recent&params%5B4%5D%5Bkey%"
-               "5D=page_job&params%5B4%5D%5Bval%5D=1&params%5B5%5D%5Bkey%"
+               f"5D=page_job&params%5B4%5D%5Bval%5D={value}&params%5B5%5D%5Bkey%"
                "5D=per_page&params%5B5%5D%5Bval%5D=9&sendEvent=false")
     headers = {
         "cookie": f"{get_cookies()}",
@@ -59,24 +59,35 @@ def prepare_post():
 def get_jobs():
 
     list_jobs = []
-    data = prepare_post()
-    response = session.request("POST", data[0], data=data[1], headers=data[2]).json()['advertsHtml']
-    soup = BeautifulSoup(response, 'lxml')
-    jobs = soup.find_all('li', class_='col-lg-4 col-md-4 col-sm-6 col-xs-12')
 
-    for job in jobs:
-        title = job.find('a', class_='aa-item').text
-        link = job.find('a', class_='aa-item')['href']
-        city = job.find('span', class_='cs-location').text.split()[-1].strip()
+    page = 1
+    flag = True
 
+    while flag:
 
-        list_jobs.append({
-            "job_title": title,
-            "job_link": link,
-            "company": "DAMEN",
-            "country": "Romania",
-            "city": city,
-        })
+        data = prepare_post(page)
+        response = session.request("POST", data[0], data=data[1], headers=data[2]).json()['advertsHtml']
+        soup = BeautifulSoup(response, 'lxml')
+        jobs = soup.find_all('li', class_='col-lg-4 col-md-4 col-sm-6 col-xs-12')
+
+        if len(jobs) > 0:
+
+            for job in jobs:
+                title = job.find('a', class_='aa-item').text
+                link = job.find('a', class_='aa-item')['href']
+                city = job.find('span', class_='cs-location').text.split()[-1].strip()
+
+                list_jobs.append({
+                    "job_title": title,
+                    "job_link": link,
+                    "company": "DAMEN",
+                    "country": "Romania",
+                    "city": city,
+                })
+            page += 1
+        else:
+            flag = False
+
     return list_jobs
 
 @update_peviitor_api
