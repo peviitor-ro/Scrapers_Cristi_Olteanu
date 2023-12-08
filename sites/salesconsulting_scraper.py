521f51b9e4d9326ab37c71273f72dd041b1eed97
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-12-08 11:52:44 +0200

    Update salesconsulting_scraper.py

--- sites/salesconsulting_scraper.py
+++ sites/salesconsulting_scraper.py
@@ -4,7 +4,6 @@
 #
 from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
-import uuid
 from bs4 import BeautifulSoup
 import requests
 
@@ -12,7 +11,7 @@ import requests
 def get_soup(url):
 
     session = requests.Session()
-    response = session.get(url,headers=DEFAULT_HEADERS)
+    response = session.get(url, headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
     return soup
 
@@ -37,29 +36,41 @@ def get_jobs():
         jobs = soup_jobs.find_all('li', class_='w-full')
 
         for job in jobs:
-            text = job.find('a',
-                            class_='flex flex-col py-6 text-center sm:px-6 hover:bg-gradient-block-base-bg focus-visible-company focus-visible:rounded')
+            text = job.find('a', class_='flex flex-col py-6 text-center sm:px-6 hover:bg-gradient-block-base-bg focus-visible-company focus-visible:rounded')
 
             if text is not None:
                 link = text['href']
                 title = job.find('span', class_='text-block-base-link sm:min-w-[25%] sm:truncate company-link-style')[
                     'title']
-                job_type = job.find('div', class_='mt-1 text-md').text.split('·')[-1].strip().split()[-1]
+                info_text = job.find('div', class_='mt-1 text-md').text.split('·')[-1].strip().split()[-1]
 
-                if 'hibrid' in job_type.lower() or 'remote' in job_type.lower():
+                if 'remote' in info_text.lower():
+                    job_type = 'remote'
+                    city = job.find('div', class_='mt-1 text-md').text.split('·')[-2].strip()
+                elif 'hybrid' in info_text.lower():
+                    job_type = 'hybrid'
                     city = job.find('div', class_='mt-1 text-md').text.split('·')[-2].strip()
                 else:
-                    city = job_type
                     job_type = 'on-site'
+                    city = info_text
 
-                if 'multiple' in city.lower():
+                if 'locations' in city.lower():
                     soup_city = get_soup(link)
-                    city = soup_city.find('dl',
+                    try:
+                        city = soup_city.find('dl',
                                           class_='md:max-w-[70%] mx-auto text-md gap-y-0 md:gap-y-5 flex flex-wrap flex-col md:flex-row company-links'
-                                          ).text.split('Locații')[1].split('Status')[0].strip().split(', ')
+                                          ).text.split('Locations')[1].split('Status')[0].strip().split('\n')[0].split(',')
+                    except:
+                        city = soup_city.find('dl',
+                                          class_='md:max-w-[70%] mx-auto text-md gap-y-0 md:gap-y-5 flex flex-wrap flex-col md:flex-row company-links'
+                                          ).text.split('Locations')[1].strip().split('\n')[0].split(',')
+
+                try:
+                    city = city.split(',')
+                except:
+                    pass
 
                 list_jobs.append({
-                    "id": str(uuid.uuid4()),
                     "job_title": title,
                     "job_link": link,
                     "company": "salesconsulting",
@@ -69,7 +80,6 @@ def get_jobs():
                 })
 
     return list_jobs
-
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
