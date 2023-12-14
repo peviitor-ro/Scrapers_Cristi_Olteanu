2a5b78520e10c369d94792c1cf1b377178dd839b
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-12-14 17:45:57 +0200

    Update arvato_scraper.py

--- sites/arvato_scraper.py
+++ sites/arvato_scraper.py
@@ -6,23 +6,22 @@ from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
 
 def get_jobs():
 
     list_jobs = []
     response = requests.get('https://jobsearch.createyourowncareer.com/Arvato_Systems/go/See-All-Jobs-en/3516301/?q=&q2=&alertId=&locationsearch=&title=&location=RO&department=',headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text,'lxml')
-    jobs = soup.find_all('tr',class_='data-row')
+    jobs = soup.find_all('tr', class_='data-row')
 
     for job in jobs:
 
-        title = job.find('a',class_='jobTitle-link').text
-        link = 'https://jobsearch.createyourowncareer.com/'+job.find('a',class_='jobTitle-link')['href']
-        city = job.find('span',class_='jobLocation').text.split(',')[0].strip()
+        title = job.find('a', class_='jobTitle-link').text
+        link = 'https://jobsearch.createyourowncareer.com/' + job.find('a', class_='jobTitle-link')['href']
+        city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "Arvato",
