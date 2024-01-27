7501fd5baf3321f7f51059ee3a3cfb8b54f84238
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-01-27 15:46:14 +0200

    Update dennemeyer_scraper.py

--- sites/dennemeyer_scraper.py
+++ sites/dennemeyer_scraper.py
@@ -14,11 +14,11 @@ def get_ids() -> tuple:
         headers=DEFAULT_HEADERS).headers
 
     play_session = re.search(r"PLAY_SESSION=([^;]+);", str(response)).group(0)
-    ts_id = re.search(r"TS014c1515=([^;]+);", str(response)).group(0)
     wday_vps = re.search(r"wday_vps_cookie=([^;]+);", str(response)).group(0)
     wd_browser_id = re.search(r"wd-browser-id=([^;]+);", str(response)).group(0)
+    cf_id = re.search(r"__cf_bm=([^;]+);", str(response)).group(0)
 
-    return play_session, ts_id, wday_vps, wd_browser_id
+    return play_session, cf_id, wday_vps, wd_browser_id
 
 
 def get_jobs():
@@ -65,6 +65,7 @@ def get_jobs():
             "city": 'Brasov'
         })
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
