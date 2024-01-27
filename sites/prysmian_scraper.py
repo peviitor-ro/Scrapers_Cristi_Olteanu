542e06fedfbf467b2336546b62137aa4b8441e53
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-01-27 15:36:46 +0200

    Update prysmian_scraper.py

--- sites/prysmian_scraper.py
+++ sites/prysmian_scraper.py
@@ -6,7 +6,6 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import re
 import requests
-import uuid
 
 session = requests.Session()
 
@@ -18,11 +17,11 @@ def get_cookies() -> tuple:
         headers=DEFAULT_HEADERS).headers
 
     play_session = re.search(r"PLAY_SESSION=([^;]+);", str(response)).group(0)
-    ts_id = re.search(r"TS014c1515=([^;]+);", str(response)).group(0)
+    cf_id = re.search(r"__cf_bm=([^;]+);", str(response)).group(0)
     wday_vps = re.search(r"wday_vps_cookie=([^;]+);", str(response)).group(0)
     wd_browser_id = re.search(r"wd-browser-id=([^;]+);", str(response)).group(0)
 
-    return play_session, ts_id, wday_vps, wd_browser_id
+    return play_session, cf_id, wday_vps, wd_browser_id
 
 
 def prepare_post():
@@ -65,7 +64,6 @@ def get_jobs():
         link = 'https://prysmiangroup.wd3.myworkdayjobs.com/en-US/Careers' + job['externalPath']
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "Prysmian",
