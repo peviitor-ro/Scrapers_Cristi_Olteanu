47c8dc6bf3d96595f6d86cd7c1c71ec5b34407df
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-01-30 08:56:56 +0200

    Update teva_scraper.py

--- sites/teva_scraper.py
+++ sites/teva_scraper.py
@@ -6,24 +6,23 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
 
 def get_jobs():
 
     response = requests.get('https://careers.teva/search/?searchby=location&createNewAlert=false&q=&locationsearch=&geolocation=&optionsFacetsDD_facility=&optionsFacetsDD_department=Romania',
                             headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
+    soup = BeautifulSoup(response.text, 'lxml')
 
-    jobs = soup.find_all('tr',class_='data-row')
+    jobs = soup.find_all('tr', class_='data-row')
     list_jobs = []
 
     for job in jobs:
-        link = 'https://careers.teva'+job.find('a',class_='jobTitle-link')['href']
-        title = job.find('a',class_='jobTitle-link').text
-        city = job.find('span',class_='jobLocation').text.split(',')[0].strip()
+        link = 'https://careers.teva'+job.find('a', class_='jobTitle-link')['href']
+        title = job.find('a', class_='jobTitle-link').text
+        city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "Teva",
@@ -32,6 +31,7 @@ def get_jobs():
         })
 
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
