1074aaeba24a0b09deb989170a569f6de565078d
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-01-31 10:35:51 +0200

    Update cmed_scraper.py

--- sites/cmed_scraper.py
+++ sites/cmed_scraper.py
@@ -6,43 +6,43 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 from bs4 import BeautifulSoup
 import requests
-import uuid
+
 
 def get_jobs():
 
-    req = requests.get('https://aixialgroup.hire.trakstar.com/#content',headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(req.text,'lxml')
-    jobs = soup.find_all('div',class_='js-card list-item list-item-clickable js-careers-page-job-list-item')
+    req = requests.get('https://aixialgroup.hire.trakstar.com/#content', headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(req.text, 'lxml')
+    jobs = soup.find_all('div', class_='js-card list-item list-item-clickable js-careers-page-job-list-item')
 
-    list_jobs=[]
+    list_jobs = []
 
     for job in jobs:
-        ty = job.find('div',class_='rb-text-4 js-job-list-opening-meta').text.split('|')[-1].strip()
+        ty = job.find('div', class_='rb-text-4 js-job-list-opening-meta').text.split('|')[-1].strip()
 
         if ty == 'Partially remote':
-            type = 'hybrid'
+            job_type = 'hybrid'
         elif ty == 'Fully remote':
-            type = 'remote'
+            job_type = 'remote'
         else:
-            type = 'on-site'
+            job_type = 'on-site'
 
         link = 'https://aixialgroup.hire.trakstar.com' + job.find('a')['href']
         title = job.find('h3').text.strip()
-        country = job.find('div',class_='rb-text-6 js-job-list-opening-loc cut-text').text.split(',')[-1].strip()
-        city = job.find('div',class_='rb-text-6 js-job-list-opening-loc cut-text').text.split(',')[0].strip()
+        country = job.find('div', class_='rb-text-6 js-job-list-opening-loc cut-text').text.split(',')[-1].strip()
+        city = job.find('div', class_='rb-text-6 js-job-list-opening-loc cut-text').text.split(',')[0].strip()
 
         if 'Romania' in country:
 
             list_jobs.append({
-                "id": str(uuid.uuid4()),
                 "job_title": title,
                 "job_link": link,
                 "company": "Cmed",
                 "country": "Romania",
                 "city": city,
-                "remote": type
+                "remote": job_type
             })
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
