9a025709986713562704655358c70ed292fcf587
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-05 10:07:51 +0200

    Update fotc_scraper.py

--- sites/fotc_scraper.py
+++ sites/fotc_scraper.py
@@ -6,41 +6,45 @@ from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
 
 def get_jobs():
 
     list_jobs = []
     response = requests.get('https://fotc.jobsoid.com/',
                             headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
-    jobs = soup.find_all('div',class_='row')
+    soup = BeautifulSoup(response.text, 'lxml')
+    jobs = soup.find_all('div', class_='row')
 
     for job in jobs:
-        text = job.find('a',class_='jobDetailsLink')
+        text = job.find('a', class_='jobDetailsLink')
         try:
             link = 'https://fotc.jobsoid.com'+text.get('href')
         except:
             link = None
 
-        if link != None:
+        if link is not None:
             title = text.text
             link = link
-            location = job.find('span',class_='r-space').text.split('/')[0].strip()
-            type = job.find('span',class_='r-space').text.split('/')[-1].strip()
+            location = job.find('span', class_='r-space').text.split('/')[0].strip()
+            city = job.find('span', class_='r-space').text.split('/')[-1].strip()
+            job_type = ''
 
             if 'Romania' in location:
+                if 'remote' in city.lower():
+                    job_type = 'remote'
+                    city = 'Bucuresti'
 
                 list_jobs.append({
-                    "id": str(uuid.uuid4()),
                     "job_title": title,
                     "job_link": link,
                     "company": "fotc",
                     "country": "Romania",
-                    "city": '',
-                    "remote": type
+                    "city": city,
+                    "remote": job_type
                 })
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
