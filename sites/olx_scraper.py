d2cc5cae605524e99a2b35caaa46d9769dcc4e0a
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-09 16:28:50 +0200

    Update olx_scraper.py

--- sites/olx_scraper.py
+++ sites/olx_scraper.py
@@ -11,29 +11,48 @@ from bs4 import BeautifulSoup
 def get_jobs():
 
     list_jobs = []
-    response = requests.get('https://jobs.eu.lever.co/olx',headers=DEFAULT_HEADERS)
+
+    response = requests.get('https://jobs.eu.lever.co/olx', headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    jobs = soup.find_all('div',class_='posting')
+    jobs = soup.find_all('div', class_='posting')
 
     for job in jobs:
         link = job.find('a', class_='posting-title')['href']
         title = job.find('h5').text
         country = job.find('span',  class_='sort-by-location posting-category small-category-label location'
-                           ).text.split(', ')[-1]
+                           ).text
         job_type = job.find('span', class_='display-inline-block small-category-label workplaceTypes'
                             ).text.split()[0]
+        location_info = job.find('span', class_='sort-by-location posting-category small-category-label location'
+                                 ).text.split()
+        cities = []
+        if 'Romania' in country:
 
-        if country == 'Romania':
+            for location in location_info:
+
+                if 'bucharest' in location.lower():
+                    cities.append('Bucuresti')
+                elif 'cluj' in location.lower():
+                    cities.append('Cluj-Napoca')
+                elif 'timisoara' in location.lower():
+                    cities.append('Timisoara')
+                elif 'iasi' in location.lower():
+                    cities.append('Iasi')
+
+            if not cities:
+                cities.append('Bucuresti')
+
             list_jobs.append({
                 "job_title": title,
                 "job_link": link,
                 "company": "Olx",
                 "country": "Romania",
-                "city": 'Bucuresti',
+                "city": cities,
                 "remote": job_type
             })
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
