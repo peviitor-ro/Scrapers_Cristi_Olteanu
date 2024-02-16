4589d7045073a03921ede206b30cae830aa541f4
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-16 10:01:36 +0200

    Update rian-partners.py

--- sites/rian-partners.py
+++ sites/rian-partners.py
@@ -6,7 +6,6 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 from bs4 import BeautifulSoup
 import requests
-import uuid
 
 
 def get_jobs():
@@ -23,10 +22,9 @@ def get_jobs():
         city = job.findNext('td',class_='location').text.split(',')[0]
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
-            "company": "rian-partners",
+            "company": "rianpartners",
             "country": "Romania",
             "city": city,
         })
@@ -42,11 +40,11 @@ def scrape_and_update_peviitor(company_name, data_list):
     return data_list
 
 
-company_name = 'rian-partners'  # add test comment
+company_name = 'rianpartners'  # add test comment
 data_list = get_jobs()
 scrape_and_update_peviitor(company_name, data_list)
 
-print(update_logo('rian-partners',
+print(update_logo('rianpartners',
                   'https://rian-partners.com/wp-content/uploads/2019/09/RIAN-partners-1.png'
                   ))
 
