af983b7d684ca0367703272f334a9cea1fded025
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-22 17:14:04 +0200

    Update riverbed_scraper.py

--- sites/riverbed_scraper.py
+++ sites/riverbed_scraper.py
@@ -1,47 +1,45 @@
 #
 # Company - > Riverbed
 # Link -> https://careers.riverbed.com/jobs?stretchUnits=MILES&stretch=10&location=Romania&lat=46&lng=25&woe=12
-from A_OO_get_post_soup_update_dec import update_peviitor_api
+from A_OO_get_post_soup_update_dec import update_peviitor_api , DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
-import uuid
+from bs4 import BeautifulSoup
+
 
 def get_jobs():
-    """
-           ... this func() make a simple requests
-           and collect data from Riverbed API.
-        """
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-    }
-
-    response = requests.get('https://careers.riverbed.com/api/jobs?stretchUnits=MILES&stretch=10&location=Romania&lat=46&lng=25&woe=12&page=1&sortBy=relevance&descending=false&internal=false',headers=headers).json()['jobs']
 
     list_jobs = []
+    url = "https://emea-apj-riverbed.icims.com/jobs/search"
+    querystring = {"ss": "1", "searchLocation": "13526", "in_iframe": "1"}
 
-    for job in response:
-        country_location = job['data']['country']
-        location = job['data']['city']
+    response = requests.request("GET", url, headers=DEFAULT_HEADERS, params=querystring)
 
-        if location == 'Home Office':
-            city = 'Cluj-Napoca'
-            remote = 'remote'
-        else:
-            city = location
-            remote = 'on-site'
+    soup = BeautifulSoup(response.text, 'lxml')
+    jobs = soup.find_all('div', class_='row')
 
+    for job in jobs:
+        link_text = job.find('a')
+        if link_text is not None:
+            link = link_text['href']
+            title = job.find('h3').text.strip()
+            location_text = job.find('div', class_='col-xs-6 header left').text
 
-        if country_location == 'Romania':
+            if 'RO-Home Office' in location_text:
+                job_type = 'remote'
+            else:
+                job_type = 'hibrid'
+
             list_jobs.append({
-                "id": str(uuid.uuid4()),
-                "job_title": job['data']['title'],
-                "job_link": job['data']['meta_data']['canonical_url'],
+                "job_title": title,
+                "job_link": link,
                 "company": "Riverbed",
                 "country": "Romania",
-                "city": city,
-                "remote": remote
+                "city": 'Cluj-Napoca',
+                "remote": job_type
             })
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
