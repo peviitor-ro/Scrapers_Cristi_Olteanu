81be54f6d787764fdeabfb250f5b3be8ae641ab9
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-03-05 17:20:19 +0200

    Update consensys_scraper.py

--- sites/consensys_scraper.py
+++ sites/consensys_scraper.py
@@ -3,29 +3,27 @@
 # Link -> https://consensys.io/open-roles?location=83558
 #
 
-from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
+from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
 
 
 def get_jobs():
     list_jobs = []
 
-    req = requests.get("https://consensys.io/open-roles?location=83558",headers=DEFAULT_HEADERS)
+    req = requests.get("https://consensys.io/open-roles?location=83558", headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(req.text, "lxml")
 
-    jobs = soup.find_all('div',class_='card-job svelte-11focjo')
+    jobs = soup.find_all('div', class_='card-job svelte-11focjo')
 
     for job in jobs:
-       link = 'https://consensys.io/' + job.find('a',class_='svelte-11focjo')['href']
-       title = job.find('h5',class_='job-title svelte-11focjo').text
-       location = job.find('div',class_='job-location svelte-11focjo').text
+       link = 'https://consensys.io/' + job.find('a', class_='svelte-11focjo')['href']
+       title = job.find('h5', class_='job-title svelte-11focjo').text
+       location = job.find('div', class_='job-location svelte-11focjo').text
 
        if 'EMEA - Remote' in location:
            list_jobs.append({
-               "id": str(uuid.uuid4()),
                "job_title": title,
                "job_link": link,
                "company": "Consensys",
@@ -35,6 +33,7 @@ def get_jobs():
            })
 
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
