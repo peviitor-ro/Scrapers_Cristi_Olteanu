d405ce6862b6c2295e7f9befe247476a1078f45a
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-03-22 20:54:45 +0200

    Update medisonpharma_scraper.py

--- sites/medisonpharma_scraper.py
+++ sites/medisonpharma_scraper.py
@@ -6,40 +6,31 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 from bs4 import BeautifulSoup
 import requests
-import uuid
+
 
 def get_jobs():
     list_jobs = []
 
-    response = requests.get('https://www.medisonpharma.com/careers/?positions',headers=DEFAULT_HEADERS)
+    response = requests.get('https://www.medisonpharma.com/careers/?positions', headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    countries = soup.find_all('div', class_='comeet-list comeet-group-name')
+    jobs = soup.find_all('a', class_='comeet-position')
 
-    for country in countries:
-        if 'Romania' in country.text:
+    for job in jobs:
+        link = job['href'].strip('//')
+        if 'romania' in link:
+            title = job.find('div', class_='comeet-position-name').text.strip()
 
-            jobs = country.find_all_next('a',class_='comeet-position',)
-            for job in jobs:
-                link = job.get('href')
-                if 'romania' in link:
-                    title = job.text.split('\n\n')[1].split('-')[0].strip('\n')
-                    try:
-                        city = job.text.split('\n\n')[1].split('-')[1].strip()
-                    except:
-                        city = 'Bucharest'
-
-
-                    list_jobs.append({
-                        "id": str(uuid.uuid4()),
-                        "job_title": title,
-                        "job_link": 'https:'+link,
-                        "company": "Medison",
-                        "country": "Romania",
-                        "city": city,
-                    })
+            list_jobs.append({
+                "job_title": title,
+                "job_link": 'https:'+link,
+                "company": "Medison",
+                "country": "Romania",
+                "city": 'Bucuresti',
+            })
 
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
