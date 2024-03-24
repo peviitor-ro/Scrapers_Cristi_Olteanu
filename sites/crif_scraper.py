6dd6531e255130ebff17c1960e70e44352f1520e
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-03-24 16:49:10 +0200

    Update crif_scraper.py

--- sites/crif_scraper.py
+++ sites/crif_scraper.py
@@ -6,23 +6,22 @@ from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
 
 def get_jobs():
     list_jobs = []
 
     res = requests.get('https://careers.crif.com/search/?q=&q2=&alertId=&locationsearch=&title=&location=Ro&facility=&shifttype=&date=',headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(res.text,'lxml')
-    jobs = soup.find_all('tr',class_='data-row')
+    soup = BeautifulSoup(res.text, 'lxml')
+    jobs = soup.find_all('tr', class_='data-row')
 
     for job in jobs:
 
-        title = job.find('a',class_='jobTitle-link').text
-        link = 'https://careers.crif.com' + job.find('a',class_='jobTitle-link')['href']
-        city = job.find('span',class_='jobLocation').text.split(',')[0].strip()
+        title = job.find('a', class_='jobTitle-link').text
+        link = 'https://careers.crif.com' + job.find('a', class_='jobTitle-link')['href']
+        city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
 
         list_jobs.append({
-                "id": str(uuid.uuid4()),
                 "job_title": title,
                 "job_link": link,
                 "company": "Crif",
@@ -31,6 +30,7 @@ def get_jobs():
         })
 
         return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
