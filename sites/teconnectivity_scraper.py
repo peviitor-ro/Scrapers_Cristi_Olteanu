466cc92d112457281b88dd60e3958ece3d698b8b
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-04-05 13:09:38 +0300

    Update teconnectivity_scraper.py

--- sites/teconnectivity_scraper.py
+++ sites/teconnectivity_scraper.py
@@ -6,15 +6,13 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
 
 
 def get_jobs():
     list_jobs = []
     response = requests.get('https://careers.te.com/search/?createNewAlert=false&q=&locationsearch=Romania&optionsFacetsDD_customfield3=&optionsFacetsDD_department='
-                            ,headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
-
+                            , headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
     jobs = soup.find_all('tr', class_='data-row')
 
     for job in jobs:
@@ -24,7 +22,6 @@ def get_jobs():
         city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "TeConnectivity",
@@ -32,6 +29,7 @@ def get_jobs():
             "city": city,
         })
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
