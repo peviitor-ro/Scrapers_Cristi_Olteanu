abfe4a37ef356b9cb72a1b364c49d06c5281ffd3
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-04-05 13:13:49 +0300

    Update spearhead_scraper.py

--- sites/spearhead_scraper.py
+++ sites/spearhead_scraper.py
@@ -4,26 +4,25 @@
 #
 from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
 from L_00_logo import update_logo
-import uuid
 import requests
 from bs4 import BeautifulSoup
+
 
 def get_jobs():
 
     list_jobs = []
 
-    response = requests.get('https://spearhead.systems/jobs',headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
-    jobs = soup.find_all('a',class_='text-decoration-none')
+    response = requests.get('https://spearhead.systems/jobs', headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+    jobs = soup.find_all('a', class_='text-decoration-none')
 
     for job in jobs:
 
         link = 'https://spearhead.systems' + job.get('href')
         title = job.find('h3').text.strip()
-        city = job.find('span',class_='w-100 o_force_ltr d-block').text.split()[-4]
+        city = job.find('span', class_='w-100 o_force_ltr d-block').text.split()[-4]
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "Spearhead",
@@ -31,6 +30,7 @@ def get_jobs():
             "city": city
         })
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
