8a895357fb15460e90ea90e8fc824fbd6e25e07e
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-04-05 13:07:10 +0300

    Update victorysquare_scraper.py

--- sites/victorysquare_scraper.py
+++ sites/victorysquare_scraper.py
@@ -5,13 +5,11 @@
 from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
-import uuid
 
 
 def get_jobs():
 
     list_jobs = []
-
     response = requests.get('https://victorysquarepartners1.recruitee.com/api/offers/',
                             headers=DEFAULT_HEADERS).json()['offers']
 
@@ -28,7 +26,6 @@ def get_jobs():
             job_type = 'on-site'
 
         list_jobs.append({
-            "id": str(uuid.uuid4()),
             "job_title": title,
             "job_link": link,
             "company": "VictorySquare",
@@ -38,6 +35,7 @@ def get_jobs():
         })
 
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
