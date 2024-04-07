8cd93ea2b4ea2cf7ee353314918c029d940e7371
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-04-07 16:00:35 +0300

    Update ses_scraper.py

--- sites/ses_scraper.py
+++ sites/ses_scraper.py
@@ -6,7 +6,7 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
-import uuid
+
 
 def get_jobs():
     list_jobs = []
@@ -15,17 +15,16 @@ def get_jobs():
                             headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    jobs = soup.find_all('tr',class_='data-row')
+    jobs = soup.find_all('tr', class_='data-row')
 
     for job in jobs:
-        link = 'https://careers.ses.com' + job.find('a',class_='jobTitle-link')['href']
-        title = job.find('a',class_='jobTitle-link').text
-        city = job.find('span',class_='jobLocation').text.split(',')[0].strip()
-        location = job.find('span',class_='jobLocation').text.split(',')[1].strip()
+        link = 'https://careers.ses.com' + job.find('a', class_='jobTitle-link')['href']
+        title = job.find('a', class_='jobTitle-link').text
+        city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
+        location = job.find('span', class_='jobLocation').text.split(',')[1].strip()
 
         if 'RO' in location:
             list_jobs.append({
-                "id": str(uuid.uuid4()),
                 "job_title": title,
                 "job_link": link,
                 "company": "Ses",
@@ -34,6 +33,7 @@ def get_jobs():
             })
 
     return list_jobs
+
 
 @update_peviitor_api
 def scrape_and_update_peviitor(company_name, data_list):
