b28fc7a864c352f61cdf2d0e7919765d49027e5c
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-05-01 18:11:12 +0300

    Update fltechnics_scraper.py

--- sites/fltechnics_scraper.py
+++ sites/fltechnics_scraper.py
@@ -6,6 +6,7 @@ from A_OO_get_post_soup_update_dec import update_peviitor_api,DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
 from bs4 import BeautifulSoup
+from _county import get_county
 
 
 
@@ -48,7 +49,9 @@ def get_jobs():
                     "job_link": link,
                     "company": "FlTechnics",
                     "country": "Romania",
-                    "city": city
+                    "city": city,
+                    "county": get_county(city),
+                    "remote": 'on-site'
                 })
     return list_jobs
 
