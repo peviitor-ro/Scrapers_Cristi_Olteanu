c2dafec72843f3c4744741f84dc73011589bc0bc
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-05-01 17:29:41 +0300

    Update hootsuite_scraper.py

--- sites/hootsuite_scraper.py
+++ sites/hootsuite_scraper.py
@@ -5,6 +5,8 @@
 from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
 from L_00_logo import update_logo
 import requests
+from _county import get_county
+from _validate_city import validate_city
 
 
 def get_jobs():
@@ -20,13 +22,16 @@ def get_jobs():
             location = office['location']
 
             if location is not None and 'Romania' in location:
-                cities.append(city)
+                cities.append(validate_city(city))
+
                 jobs_list.append({
                     "job_title": job['title'],
                     "job_link": job['absolute_url'].replace('jobs', 'job'),
                     "company": "hootsuite",
                     "country": "Romania",
                     "city": cities,
+                    "county": get_county(cities),
+                    "remote": 'on-site'
                 })
 
     return jobs_list
