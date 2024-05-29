409c91ee83d367459c600d700171ad594e08d8a5
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-05-29 17:43:20 +0300

    Update assaabloy_scraper.py

--- sites/assaabloy_scraper.py
+++ sites/assaabloy_scraper.py
@@ -31,7 +31,7 @@ def get_jobs():
             "country": "Romania",
             "city": 'Bucuresti',
             "county": 'Bucuresti',
-            "remote": 'on=site'
+            "remote": 'on-site'
         })
     return list_jobs
 
