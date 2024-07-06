6c678405f9b91abf564c0d188eac9c228b44a1f4
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-07-06 18:42:04 +0300

    Update L_00_logo.py

--- sites/L_00_logo.py
+++ sites/L_00_logo.py
@@ -12,7 +12,8 @@ def update_logo(id: str, logo_url: str):
     Update Logo
     """
     headers = {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
     }
     url = "https://api.peviitor.ro/v1/logo/add/"
     data = json.dumps([{"id": id, "logo": logo_url}])
