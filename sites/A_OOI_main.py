2e97e86f4f44711d1961c3f441d0a75d98f68912
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-02-24 18:34:47 +0200

    Update .gitignore, requirements, and add nefab_scraper; enhance A_OOI_main.py error handling

--- sites/A_OOI_main.py
+++ sites/A_OOI_main.py
@@ -44,10 +44,14 @@ path = os.path.dirname(os.path.abspath(__file__))
 
 for site in os.listdir(path):
     if site.endswith('.py') and site not in exclude:
-        action = subprocess.run(['python', os.path.join(path, site)], capture_output=True)
-        if action.returncode != 0:
-            errors = action.stderr.decode('utf-8')
-            print("Error in " + site)
-            print(errors)
-        else:
-            print("Success scraping " + site)
+        try:
+            action = subprocess.run(['python', os.path.join(path, site)], capture_output=True, timeout=60)
+            if action.returncode != 0:
+                errors = action.stderr.decode('utf-8')
+                print("Error in " + site)
+                print(errors)
+            else:
+                print("Success scraping " + site)
+                print(action.stdout.decode('utf-8'))
+        except subprocess.TimeoutExpired:
+            print("Timeout in " + site)
