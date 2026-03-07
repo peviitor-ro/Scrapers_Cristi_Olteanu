5a20e5fb0b752480b00ee309872a96f80939d721
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-03-07 17:48:32 +0200

    Refactor Consensys scraper to update job extraction logic and remove location-specific URL

--- sites/consensys_scraper.py
+++ sites/consensys_scraper.py
@@ -1,6 +1,6 @@
 #
 # Company - > Consensys
-# Link -> https://consensys.io/open-roles?location=83558
+# Link -> https://consensys.io/open-roles
 #
 
 from A_OO_get_post_soup_update_dec import update_peviitor_api, DEFAULT_HEADERS
@@ -12,25 +12,31 @@ from bs4 import BeautifulSoup
 def get_jobs():
     jobs_list = []
 
-    req = requests.get("https://consensys.io/open-roles?location=83558", headers=DEFAULT_HEADERS)
+    req = requests.get("https://consensys.io/open-roles", headers=DEFAULT_HEADERS)
     soup = BeautifulSoup(req.text, "lxml")
-    jobs = soup.find_all('a', class_='card-job svelte-rgojh5')
 
-    for job in jobs:
-       link = 'https://consensys.io/' + job.get('href')
-       title = job.find('h5').text
-       location = job.find('div', class_='job-location svelte-rgojh5').text
+    links = soup.find_all('a', href=lambda x: x and '/open-roles/' in x)
 
-       if 'EMEA - Remote' in location or 'GLOBAL - Remote' in location:
-           jobs_list.append({
-               "job_title": title,
-               "job_link": link,
-               "company": "Consensys",
-               "country": "Romania",
-               "city": 'Cluj-Napoca',
-               "county": 'Cluj',
-               "remote": 'remote'
-           })
+    for link in links:
+        href = link.get('href')
+        text = link.text.strip()
+        
+        if 'EMEA - Remote' in text or 'GLOBAL - Remote' in text:
+            # Extract title - get everything before the first location
+            title = text.split(',')[0].strip()
+            # Remove location suffixes from title
+            title = title.replace('UNITED STATES - Remote', '').replace('CANADA - Remote', '').replace('EMEA - Remote', '').replace('GLOBAL - Remote', '').replace('  ', ' ').strip()
+            title = title.rstrip('-').strip()
+            
+            jobs_list.append({
+                "job_title": title,
+                "job_link": 'https://consensys.io' + href,
+                "company": "Consensys",
+                "country": "Romania",
+                "city": 'Bucuresti',
+                "county": 'Bucuresti',
+                "remote": 'remote'
+            })
     return jobs_list
 
 
