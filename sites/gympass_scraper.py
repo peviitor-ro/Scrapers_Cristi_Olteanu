eb64820893c16c475b808d54f750a37c4653e490
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-03-10 00:14:39 +0200

    Refactor Gympass scraper to use API for job listings, enhance location handling, and improve city translation

--- sites/gympass_scraper.py
+++ sites/gympass_scraper.py
@@ -1,44 +1,56 @@
 
 #
-#  Company - > Gympass
+#  Company - > Gympass (now Wellhub)
 # Link -> https://boards.greenhouse.io/gympass
 #
 import requests
 from bs4 import BeautifulSoup
 from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
 from L_00_logo import update_logo
+from _county import get_county
 
 
 def get_jobs():
 
     list_jobs = []
 
-    response = requests.get('https://boards.greenhouse.io/gympass', headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text, 'lxml')
-    jobs = soup.find_all('div', class_='opening')
+    response = requests.get('https://boards-api.greenhouse.io/v1/boards/gympass/jobs', headers=DEFAULT_HEADERS)
+    data = response.json()
 
-    for job in jobs:
-        link = 'https://boards.greenhouse.io/' + job.find('a')['href']
-        title = job.find('a').text
-        location = job.find('span', class_='location').text
-
-        if 'Remote' in location:
-            type = 'remote'
-        elif 'Hybrid' in location:
-            type = 'hybrid'
-        else:
-            type = 'on-site'
-
+    for job in data.get('jobs', []):
+        location = job.get('location', {}).get('name', '')
+        
         if 'Romania' in location:
-            city = job.find('span', class_='location').text.split()[1].strip('(')
+            title = job.get('title', '')
+            link = job.get('absolute_url', '')
+            
+            if 'Remote' in location:
+                remote_type = 'remote'
+            elif 'Hybrid' in location:
+                remote_type = 'hybrid'
+            else:
+                remote_type = 'on-site'
+            
+            location_part = location.split('(')[1].strip(')') if '(' in location else location
+            city = location_part.split(' - ')[0].strip() if ' - ' in location_part else location.strip()
+            if city == 'Romania':
+                city = location.split('(')[0].strip()
+            
+            city_translations = {
+                'Bucharest': 'București',
+            }
+            city = city_translations.get(city, city)
+            
             list_jobs.append({
                 "job_title": title,
                 "job_link": link,
                 "company": "Gympass",
                 "country": "Romania",
                 "city": city,
-                "remote": type
+                "county": get_county(city),
+                "remote": remote_type
             })
+
     return list_jobs
 
 
