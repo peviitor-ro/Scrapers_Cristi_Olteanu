9171a4691418e329f86f2c380aa18c03b4a8ea93
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-03-12 00:01:12 +0200

    Refactor justrocket scraper to use JSON API for job listings, enhance location handling, and improve remote work classification

--- sites/justrocket_scraper.py
+++ sites/justrocket_scraper.py
@@ -5,40 +5,58 @@
 from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS,update_peviitor_api
 from L_00_logo import update_logo
 import requests
-from bs4 import BeautifulSoup
+import re
+import json
+from _county import get_county
 
 
 def get_jobs():
 
     list_jobs = []
-    response = requests.get('https://join.com/companies/justrocket'
-                            ,headers=DEFAULT_HEADERS)
-    soup = BeautifulSoup(response.text,'lxml')
-    jobs = soup.find_all('a',class_='sc-fbbtMj kBtloA JobTile___StyledJobLink-sc-989ef686-0 kYEttm JobTile___StyledJobLink-sc-989ef686-0 kYEttm')
-
-    for job in jobs:
-
-        location = job.find('div', class_='sc-hLseeU JobTile-elements___StyledText-sc-e7e7aa1d-4 fyJRsY kPLurW').text
-
-        if 'hybrid' in location:
-            type = 'hybrid'
-        elif 'remote' in location:
-            type = 'remote'
-        else:
-            type = 'on-site'
-
-        link = job.get('href')
-        city = job.find('div',class_='sc-hLseeU JobTile-elements___StyledText-sc-e7e7aa1d-4 fyJRsY kPLurW').text.split(',')[0]
-        title = job.find('h3').text
-
-        list_jobs.append({
-            "job_title": title,
-            "job_link": link,
-            "company": "JUSTROCKET",
-            "country": "Romania",
-            "city": city,
-            "remote": type
-        })
+    response = requests.get('https://join.com/companies/justrocket', headers=DEFAULT_HEADERS)
+    
+    pattern = r'"jobs":\{"items":\[(.*?)\],"pagination"'
+    match = re.search(pattern, response.text, re.DOTALL)
+    
+    if match:
+        items_str = '[' + match.group(1) + ']'
+        
+        try:
+            jobs = json.loads(items_str)
+            
+            for job in jobs:
+                city = job.get('city', {}).get('cityName', '')
+                country = job.get('city', {}).get('countryName', '')
+                
+                if country == 'Romania':
+                    title = job.get('title', '')
+                    workplace_type = job.get('workplaceType', '')
+                    
+                    if workplace_type == 'HYBRID':
+                        remote_type = 'hybrid'
+                    elif workplace_type == 'REMOTE':
+                        remote_type = 'remote'
+                    else:
+                        remote_type = 'on-site'
+                    
+                    city_translations = {
+                        'Cluj-Napoca': 'Cluj-Napoca',
+                    }
+                    city = city_translations.get(city, city)
+                    
+                    link = f"https://join.com/companies/justrocket/{job.get('idParam', '')}"
+                    
+                    list_jobs.append({
+                        "job_title": title,
+                        "job_link": link,
+                        "company": "JUSTROCKET",
+                        "country": "Romania",
+                        "city": city,
+                        "county": get_county(city),
+                        "remote": remote_type
+                    })
+        except Exception as e:
+            print(f"Error parsing jobs: {e}")
 
     return list_jobs
 
@@ -59,3 +77,4 @@ scrape_and_update_peviitor(company_name, data_list)
 print(update_logo('JUSTROCKET',
                   'https://cdn.join.com/5f5f31e58d5d6100012b61c6/justrocket-logo-l.jpg'
                   ))
+
